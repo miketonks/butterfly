341e834794f61447e3e65cd0886de4b8e27eec2f
Mike Tonks <michael.tonks@headforwards.com>
2017-10-12 19:36:05 +0100

    Creaded first mod with butterfly and rainbow blocks

---
+++ init.lua
@@ -0,0 +1,14 @@
+minetest.register_node("tutorial:decowood", {
+	tiles = {"tutorial_decowood.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+})
+
+minetest.register_node("tutorial:butterfly", {
+	tiles = {"tutorial_butterfly.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+})
+
+minetest.register_node("tutorial:rainbow", {
+	tiles = {"tutorial_rainbow.png"},
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3},
+})
